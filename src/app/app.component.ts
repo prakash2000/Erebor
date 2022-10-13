7d0adc2cf9e0843b79100a2d25ccafa8d0c2e8c3
prakash2000 <prakash1391@gmail.com>
2022-10-13 22:40:16 +0530

    add btn changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Erebor Wealth App';
+  public showPopup = false;
+  public addItem(){
+    this.showPopup = true;
+  }
 }
