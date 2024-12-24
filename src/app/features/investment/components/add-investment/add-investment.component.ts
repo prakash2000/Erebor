e56b4552f797c9cdbf2fed2e715d7df97c947b0f
prakash2000 <prakash1391@gmail.com>
2024-12-24 23:56:12 +0530

    data changes

--- src/app/features/investment/components/add-investment/add-investment.component.ts
+++ src/app/features/investment/components/add-investment/add-investment.component.ts
@@ -25,8 +25,8 @@ export class AddInvestmentComponent implements OnInit {
     this.recentInvestments.push({category: "Equity", date: '05-05-24', amount:8000, note:'Equity Purchase'});
     this.recentInvestments.push({category: "Equity", date: '15-05-24', amount:6000, note:'Equity Purchase'});
     this.recentInvestments.push({category: "Coin", date: '15-05-24', amount:5000, note:'MF Purchase'});
-    this.recentInvestments.push({category: "Coin", date: '25-05-24', amount:7000, note:'MF Purchase'});
-    this.recentInvestments.push({category: "NPS", date: '25-05-24', amount:4000, note:'NPS Invest'});
+    this.recentInvestments.push({category: "Coin", date: '25-08-24', amount:7000, note:'MF Purchase'});
+    this.recentInvestments.push({category: "NPS", date: '25-12-24', amount:4000, note:'NPS Invest'});
     console.log(this.recentInvestments);
     // this.commonService.showPopup.subscribe(value => {
     //   this.showPopup = value;
